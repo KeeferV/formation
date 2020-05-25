5649d1338af091653d404511f7ab70e838cc2f27
keith <keith.vernon@batiactugroupe.com>
2020-05-25 13:51:55 +0200

    mods

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
 let os = require('os')
 var fs = require('fs')
 
-fs.readFile(`${__dirname}/products.json`, (err, data) => {
-  if (err) {
-    throw err;
-  }
-  try {
-    var books = JSON.parse(data);
-  } catch (err) {
-    throw err;
-  }
-  getAllProducts(books);
+getAllProducts();
 
-})
+function getAllProducts() {
+  fs.readFile(`${__dirname}/products.json`, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    try {
+      var books = JSON.parse(data);
+    } catch (err) {
+      throw err;
+    }
+    books.forEach(book => {
+      console.log(`${book.id} - ${book.name} / ${book.EUR_price} / ${book.orders_counter}`);
+    })
 
-
-function getAllProducts(books) {
-  books.forEach(book => {
-    console.log(`${book.id} - ${book.name} / ${book.EUR_price} / ${book.orders_counter}`);
   })
+
 }
+/*
+function orderProductById(id) {
+  fs.readFile(`${__dirname}/products.json`, (err, data) => {
+    if (err) {
+      throw err;
+    }
+    try {
+      var books = JSON.parse(data);
+    } catch (err) {
+      throw err;
+    }
+    books.forEach(book => {
+      if (book.id == id) {
+        book.orders_counter++;
+        data = JSON.toString(books)
+
+      }
+    })
+  }
+}
+*/
