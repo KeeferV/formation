89717328f08a3219dfa5f6586187ca5fbe77f935
keith <keith.vernon@batiactugroupe.com>
2020-05-25 13:35:51 +0200

    mods

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ fs.readFile(`${__dirname}/products.json`, (err, data) => {
   if (err) {
     throw err;
   }
-  let books = JSON.parse(data);
+  try {
+    let books = JSON.parse(data);
+  } catch (err) {
+    throw err;
+  }
   for (i in books) {
     console.log(books[i].name);
   }
