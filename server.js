67bcffd79c3efb33c0c868628e81b0ead84398d4
keith <keith.vernon@batiactugroupe.com>
2020-05-26 10:08:11 +0200

    mods

--- server.js
+++ server.js
@@ -14,23 +14,44 @@ const server = http.createServer(function (req, res) {
   res.end()
 }).listen(8082);
 */
+var fs = require('fs')
 var path = require('path')
 
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 //app.use
 
 app.get('/', (req, res) => {
-  //var o = {a: "Hi"}
-  res.render("homepage");
+  getAllProducts((err, products) => {
+    if (err) {
+      throw err;
+    }
+    res.render("index", {products: products});
+  });
 })
 app.get('/about', (req, res) => res.send('About us'))
 app.get('/contact', (req, res) => res.send('Contact us!'))
 app.get('/*', (req, res) => res.send('Other pages'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+
+function getAllProducts(callback) {
+  fs.readFile(path.join(__dirname, "products.json"), "utf8", (err, contents) => {
+    if (err) {
+      return callback(err)
+    }
+    try {
+      var products = JSON.parse(contents).products;
+      return callback(null, products)
+    } catch (error) {
+      return callback(error)
+    }
+  })
+}
+
