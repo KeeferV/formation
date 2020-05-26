c31b4ffa63cd88b3eea427b7866047f719977efe
keith <keith.vernon@batiactugroupe.com>
2020-05-26 15:01:15 +0200

    mods

--- server.js
+++ server.js
@@ -1,24 +1,13 @@
-/*
-var http = require('http')
-
-const server = http.createServer(function (req, res) {
-  const url = req.url;
-  if (url === '/about') {
-    res.write('<h1>about us</h1>');
-  } else if (url === "/contact") {
-    res.write('<h1>Contact us</h1>');
-  } else {
-    res.write(req.method)
-    res.write('Hello world');
-  }
-  res.end()
-}).listen(8082);
-*/
 var fs = require('fs')
 var path = require('path')
 var mongoose = require('mongoose')
 var products = require('./models/products.js');
-mongoose.connect('mongodb://localhost:27017/database', {useNewUrlParser: true})
+
+mongoose.connect('mongodb://localhost:27017/database', {
+  "useNewUrlParser": true,
+  "useFindAndModify": false,
+  "useUnifiedTopology": true
+})
 mongoose
     .connection
     .on('connected', function () {
