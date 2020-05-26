89c437be5566e3a05fed05bc323f967a9c3eccc9
keith <keith.vernon@batiactugroupe.com>
2020-05-26 08:45:08 +0200

    mods

--- server.js
+++ server.js
@@ -14,12 +14,23 @@ const server = http.createServer(function (req, res) {
   res.end()
 }).listen(8082);
 */
+var path = require('path')
+
 const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+//app.use
+
+app.get('/', (req, res) => {
+  //var o = {a: "Hi"}
+  res.render("homepage");
+})
 app.get('/about', (req, res) => res.send('About us'))
-app.get('/contact', (req, res) => res.send('Contact us'))
+app.get('/contact', (req, res) => res.send('Contact us!'))
+app.get('/*', (req, res) => res.send('Other pages'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
