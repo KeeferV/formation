02e5b3e676a0f1dadabd74de79003a51815d07a8
keith <keith.vernon@batiactugroupe.com>
2020-05-28 09:27:34 +0200

    form

---
+++ saved/orig.js
@@ -0,0 +1,32 @@
+//var x = "Hello ! I am node.js";
+//console.log(x)
+//process.stdout.write("'comme ici");
+//stdin
+
+/*
+ var readline = require('readline');
+ var rl = readline.createInterface({input: process.stdin, output: process.stdout, terminal: false});
+
+ console.log(__dirname, __filename);
+
+ console.log(os.uptime());
+ console.log(process.platform);
+
+ rl.on('line', function (line) {
+ console.log(line);
+ })
+
+ let files = fs.readdirSync(`${__dirname}/files/`);
+
+ files.forEach((file) => {
+ //console.log(file);
+ //const content = fs.readFileSync(`${__dirname}/files/${file}`, 'UTF8')
+ //console.log(content);
+ fs.readFile(`${__dirname}/files/${file}`, 'UTF8', (err, file) => {
+ console.log(err, file);
+ })
+
+ })
+ */
+
+//fs.readFile(`${__dirname}/products.json`, 'UTF8', (err, file) => {
